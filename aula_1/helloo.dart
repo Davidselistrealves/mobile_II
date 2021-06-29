79d7edea8ba4c530b4ea6f5c82b78aff0f1de428
Davidselistrealves <83313244+Davidselistrealves@users.noreply.github.com>
2021-06-29 19:09:19 -0300

    Add files via upload

---
+++ aula_1/helloo.dart
@@ -0,0 +1,19 @@
+// tipo nome
+void main() {
+
+print("hello world");
+print(true);
+
+//comentario de linha
+
+/*
+Um comentário
+de varias
+linhas
+*/
+
+/// ou /** */
+///  @param int numero informado
+///  @return List<int> numeros da sequência
+
+}
